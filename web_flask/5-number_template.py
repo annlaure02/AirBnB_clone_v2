6b101616e1fce0e55ba360e6285e61f1723adcff
annlaure02 <alaure.g02@gmail.com>
2023-04-03 17:29:53 +0200

    added 5-number_template file and number.html file, task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask, render_template
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ display “Hello HBNB!” """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ display “HBNB” """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """
+    display “C ” followed by the value of the text variable
+    and replace underscore with a space
+    """
+    text = text.replace("_", " ")
+    return f"C {escape(text)}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>")
+def python_text(text="is cool"):
+    """
+    display “Python ”, followed by the value of the text variable
+    and replace underscore with a space
+    """
+    text = text.replace("_", " ")
+    return f"Python {escape(text)}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_n(n):
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template_n(n):
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
