c55840663170051edf82956179010d8fc7c5d084
annlaure02 <alaure.g02@gmail.com>
2023-04-03 16:39:12 +0200

    fixed pycodestyle trailing space line 24 - 35

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,7 +21,7 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
-    """ 
+    """
     display “C ” followed by the value of the text variable
     and replace underscore with a space
     """
@@ -32,7 +32,7 @@ def c_text(text):
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>")
 def python_text(text="is cool"):
-    """ 
+    """
     display “Python ”, followed by the value of the text variable
     and replace underscore with a space
     """
